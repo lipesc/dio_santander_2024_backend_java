45c49c9951dbed5f6deaee649cc44038c29d75e8
lipesc <felipedrhouse@hotmail.com>
2024-06-02 20:43:44 -0300

    desafio

---
+++ src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal {
+  public static void main(String[] args) {
+    //todo: Conhecer e implementar as classe Scanner 
+
+    // exibir as mensagem para o usuario
+
+    //obter pelo Scanner os dados do usuario
+
+    //Exibir a mensagem conta criada
+
+    
+  
+  }
+}
