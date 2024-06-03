f18c482a22b679e3ac078c608a3741b40f960213
lipesc <felipedrhouse@hotmail.com>
2024-06-02 21:17:36 -0300

    desafio-terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,14 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
   public static void main(String[] args) {
-    //todo: Conhecer e implementar as classe Scanner 
+    // todo: Conhecer e implementar as classe Scanner
 
     // exibir as mensagem para o usuario
 
-    //obter pelo Scanner os dados do usuario
+    // obter pelo Scanner os dados do usuario
 
-    //Exibir a mensagem conta criada
+    // Exibir a mensagem conta criada
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
 
-    
-  
+    int numero;
+    String agencia;
+    String nomeCliente;
+    double saldo = 2050.50;
+
+    System.out.println("Por favor, digite o número da conta! ");
+    numero = scanner.nextInt();
+    scanner.nextLine();// pula a quebra de linha
+
+    System.out.println("Digite a agencia! ");
+    agencia = scanner.nextLine();
+
+    System.out.println("Digite o seu Nome! ");
+    nomeCliente = scanner.nextLine();
+    scanner.close();
+
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
   }
 }
