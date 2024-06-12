65c9ded303f05176c4816306c761f5c0f2d54af5
Felipe Sampaio <132011920+lipesc@users.noreply.github.com>
2024-06-12 12:40:15 -0300

    --> new Update Iphone.java
    tess

--- poodio/Iphone.java
+++ poodio/Iphone.java
@@ -3,13 +3,8 @@
 
 ### Modelagem e Diagramação de um Componente iPhone
 
-Neste desafio, você será responsável por modelar e diagramar a representação UML do componente iPhone, abrangendo suas funcionalidades como Reprodutor Musical, Aparelho Telefônico e Navegador na Internet.
-
-#### Contexto
-Com base no vídeo de lançamento do iPhone de 2007 (link abaixo), você deve elaborar a diagramação das classes e interfaces utilizando uma ferramenta UML de sua preferência. Em seguida, implemente as classes e interfaces no formato de arquivos `.java`.
-
-[Lançamento iPhone 2007](https://www.youtube.com/watch?v=9ou608QQRq8)
-- Minutos relevantes: 00:15 até 00:55
+Neste desafio, você será responsável por modelar e diagramar a representação UML do componente iPhone, 
+abrangendo suas funcionalidades como Reprodutor Musical, Aparelho Telefônico e Navegador na Internet.
 
 #### Funcionalidades a Modelar
 1. **Reprodutor Musical**
